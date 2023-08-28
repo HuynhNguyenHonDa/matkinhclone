54a1d5323fbd02dade9e6d191d637b0427095af6
unknown <huynhonda57@gmail.com>
2023-08-28 14:09:55 +0700

    api

--- frontend/src/common/ApiConfig.ts
+++ frontend/src/common/ApiConfig.ts
@@ -1,4 +1,6 @@
 export enum ApiConfig {
+  // prod_baseUrl = "https://103.90.227.238/",
+  // local_baseUrl = "https://localhost:8000/",
   prod_baseUrl = "https://103.90.227.238/",
   local_baseUrl = "https://localhost:8000/",
 }
